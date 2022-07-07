52a6060a3c65a9db1f80e1924b692dd7be27ff15
Stanislau Marudau <marudov.stas@gmail.com>
2022-07-07 22:39:51 +0300

    Third try

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -60,7 +60,7 @@ public class ArrayTasks {
      */
     public int findIndexOfNumber(int[] arr, int number) {
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == number)
+            if (arr[i] == number && number!=arr.length)
                 return i;
         }
         return -1;
